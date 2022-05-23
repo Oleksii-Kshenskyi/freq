5721fe2db2efc73d5d186b0de507213ceb5611b1
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-05-23 23:08:38 +0300

    0.1.0 small hotfix: default UTF-8 encoding.

--- src/main/org/einax/freq/Freq.scala
+++ src/main/org/einax/freq/Freq.scala
@@ -7,6 +7,8 @@ import org.einax.freq.analyzers.Analysis
 
 object Freq {
     def main(args: Array[String]): Unit = {
+        System.setProperty("file.encoding", "UTF-8")
+
         val extracted_text = ChooseExtractor.basedOnFileType(args(0)).extractText()
         val freq_analysis = ChooseAnalyzer.basedOnAnalysisType(Analysis.FREQUENCY_ANALYSIS_TYPE).analyze(extracted_text)
         ChooseReporter.basedOnAnalysisType(Analysis.FREQUENCY_ANALYSIS_TYPE).reportToFile(args(1), freq_analysis)
