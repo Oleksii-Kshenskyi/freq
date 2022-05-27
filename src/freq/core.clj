b409c4df958da928590f4a62c6c328619e702b4c
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2022-05-27 21:23:48 +0300

    Dummy Clojure project.

---
+++ src/freq/core.clj
@@ -0,0 +1,7 @@
+(ns freq.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
---
+++ test/freq/core_test.clj
@@ -0,0 +1,7 @@
+(ns freq.core-test
+  (:require [clojure.test :refer :all]
+            [freq.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
